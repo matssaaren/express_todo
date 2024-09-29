6e7e740808f356f022e5c8c96b9e2018251091c4
Mats Saaren <mats.saaren@voco.ee>
2024-09-29 20:52:04 +0300

    Done

--- index.js
+++ index.js
@@ -1,115 +1,161 @@
-const express = require('express')
-const bp = require("body-parser");
-const app = express()
-const fs = require('fs');
-const { type } = require('os');
+const express = require('express');
+const app = express();
+const fs = require('fs').promises;
+const path = require('path');
 
 // body-parser allows us to read the body of a request
-app.use(bp.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // ejs
-const path = require('path');
-const { rejects } = require('assert');
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-const readFile = (filename) => {
-    return new Promise((resolve, reject) => {
-        // get the tasks from the file
-        fs.readFile(filename, 'utf8', (err, data) => {
-            if (err) {
-                reject(err)
-            }
-            // tasks list data from file
-            const tasks = JSON.parse(data)
-            resolve(tasks)
-        })
-    })
-}
+const readFile = async (filePath) => {
+  const data = await fs.readFile(filePath, 'utf8');
+  return JSON.parse(data);
+};
 
-const writeFile = (filename, data) => {
-    return new Promise((resolve, reject) => {
-        // write the tasks to the file
-        fs.writeFile(filename, data, (err) => {
-            if (err) {
-                console.error(err)
-                reject(err)
-            }
-            resolve(true)
-        })
-    })
-}
+const writeFile = async (filePath, data) => {
+  await fs.writeFile(filePath, data, 'utf8');
+};
 
-app.get('/', (req, res) => {
-    console.log('Loading tasks')
-    readFile('tasks.json').then((tasks) => {
-        console.log(tasks)
-        res.render('index', {tasks: tasks, error: null})
-    });
+app.get('/', async (req, res) => {
+  try {
+    const tasks = await readFile('tasks.json');
+    res.render('index', { tasks, edit: null, error: null });
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error reading tasks' });
+  }
 });
 
-app.post('/', (req, res) => {
-    console.log('Adding task')
-    let error = null
-    if(req.body.task.trim().length === 0) {
-        error = 'Please insert correct task data'
-        readFile('./tasks.json').then(tasks => {
-            res.render('index', {
-                tasks: tasks,
-                error: error
-            })
-        })
-    } else {
-        readFile('tasks.json').then((tasks) => {
-                let index
-                // console.log(tasks)
-                // console.log(tasks.length)
-                // console.log(tasks[1])
-                if (tasks.length === 0) {
-                    index = 0
-                } else {
-                    // console.log(tasks[tasks.lenght - 1])
-                    index = tasks[tasks.length - 1].id + 1
-                }
-                const newTask = {
-                    'id': index,
-                    'task': req.body.task,
-                }
-                console.log(newTask)
-                tasks.push(newTask)
-                console.log(tasks)
-                data = JSON.stringify(tasks, null, 2)
-                console.log(data)
-                newTaskText = req.body.task
-                if (newTaskText != "") {
-                    tasks.push(newTask)
-                }
-                writeFile('tasks.json', data)
-                res.redirect('/')
-    })}
-})
+app.get('/update-task/:index', async (req, res) => {
+  const index = parseInt(req.params.index);
+  try {
+    const tasks = await readFile('tasks.json');
+    res.render('index', { tasks, edit: index, error: null });
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error reading tasks' });
+  }
+});
 
-app.get('/delete-task/:taskId', (req, res) => {
-    let deleteTaskId = parseInt(req.params.taskId)
-    console.log('Deleting task id: ' + deleteTaskId)
-    readFile('tasks.json').then((tasks) => {
-        tasks.forEach((task, index) => {
-            if (task.id === deleteTaskId) {
-                tasks.splice(index, 1)
-            }
-        });
-        data = JSON.stringify(tasks, null, 2)
-        writeFile('tasks.json', data)
-        res.redirect('/')
-    })
-})
-app.get('/clear-all/', (req, res) => {
-    console.log('Clearing all tasks')
-    tasks = []
-    data = JSON.stringify(tasks, null, 2)
-    writeFile('tasks.json', data)
-    res.redirect('/')
-})
+app.post('/update-task/', async (req, res) => {
+  const index = parseInt(req.body.edit);
+  const updatedTask = req.body.task;
+  try {
+    const tasks = await readFile('tasks.json');
+    if (index >= 0 && index < tasks.length) {
+      tasks[index].task = updatedTask;
+      await writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+      res.redirect('/');
+    } else {
+      res.render('index', { tasks, edit: null, error: 'Invalid task index' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error updating task' });
+  }
+});
+
+app.post('/', async (req, res) => {
+  const newTask = req.body.task;
+  try {
+    const tasks = await readFile('tasks.json');
+    tasks.push({ id: tasks.length + 1, task: newTask });
+    await writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error adding task' });
+  }
+});
+
+app.get('/delete-task/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    let tasks = await readFile('tasks.json');
+    tasks = tasks.filter(task => task.id !== id);
+    await writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error deleting task' });
+  }
+});
+
+app.get('/clear-all/', async (req, res) => {
+  try {
+    await writeFile('tasks.json', JSON.stringify([], null, 2));
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error clearing tasks' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+app.post('/', async (req, res) => {
+  const newTask = req.body.task;
+  try {
+    const tasks = await readFile('tasks.json');
+    if (newTask.trim().length === 0) {
+      const error = 'Please insert correct task data';
+      res.render('index', { tasks, error });
+    } else {
+      let index;
+      if (tasks.length === 0) {
+        index = 0;
+      } else {
+        index = tasks[tasks.length - 1].id + 1;
+      }
+      const newTaskObj = {
+        id: index,
+        task: newTask,
+      };
+      tasks.push(newTaskObj);
+      await writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+      res.redirect('/');
+    }
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], error: 'Error adding task' });
+  }
+});
+
+app.get('/update-task/:taskId', async (req, res) => {
+  const updateTaskId = parseInt(req.params.taskId);
+  try {
+    const tasks = await readFile('tasks.json');
+    const taskToEdit = tasks.find(task => task.id === updateTaskId);
+    res.render('index', { tasks, edit: updateTaskId, error: null });
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error reading tasks' });
+  }
+});
+
+app.post('/update-task/', async (req, res) => {
+  const index = parseInt(req.body.edit);
+  const updatedTask = req.body.task;
+  try {
+    const tasks = await readFile('tasks.json');
+    if (index >= 0 && index < tasks.length) {
+      tasks[index].task = updatedTask;
+      await writeFile('tasks.json', JSON.stringify(tasks, null, 2));
+      res.redirect('/');
+    } else {
+      res.render('index', { tasks, edit: null, error: 'Invalid task index' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render('index', { tasks: [], edit: null, error: 'Error updating task' });
+  }
+});
+
 app.listen(3001, () => {
-    console.log('http://localhost:3001')
-})
+  console.log('http://localhost:3001');
+});
